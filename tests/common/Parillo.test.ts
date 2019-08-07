e1f3d19e8e5e2bd60ab26ff4c79acd079906e9fb
David FLAUGERE <dflaugere@kairios.fr>
2019-08-07 16:38:57 +0200

    feat(algorithm): Parillo

---
+++ tests/common/Parillo.test.ts
@@ -0,0 +1,8 @@
+import Parillo from '../../src/common/Parillo';
+
+describe('Parillo', function () {
+  it('calculate', function () {
+    let result = Parillo.calculate(5, 5, 5, 5, 5, 5, 5, 5, 5, 80);
+    expect(result.toFixed(2)).toBe("6.89");
+  });
+});
